15a90d76f5824cb37665edd813e8a6c6d94ca6c1
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-10-01 22:58:08 +0530

    actor and critic network implemented

---
+++ PPO/agent.py
@@ -0,0 +1,50 @@
+import os
+import glob
+import time
+from datetime import datetime
+
+import torch
+import torch.nn as nn
+from torch.distributions import MultivariateNormal
+from torch.distributions import Categorical
+import numpy as np
+
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim, has_continuous_action_space, action_std_init):
+        super(ActorCritic, self).__init__()
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.has_continuous_action_space = has_continuous_action_space
+
+        if has_continuous_action_space:
+            self.action_dim = action_dim
+            self.action_var = torch.full((action_dim,), action_std_init * action_std_init).to(self.device)
+
+        # actor
+        if has_continuous_action_space :
+            self.actor = nn.Sequential(
+                            nn.Linear(state_dim, 32),
+                            nn.Tanh(),
+                            nn.Linear(32, 32),
+                            nn.Tanh(),
+                            nn.Linear(32, action_dim),
+                            nn.Tanh()
+                        )
+        else:
+            self.actor = nn.Sequential(
+                            nn.Linear(state_dim, 32),
+                            nn.Tanh(),
+                            nn.Linear(32, 32),
+                            nn.Tanh(),
+                            nn.Linear(32, action_dim),
+                            nn.Softmax(dim=-1)
+                        )
+            
+        self.critic = nn.Sequential(
+                        nn.Linear(state_dim, 64),
+                        nn.Tanh(),
+                        nn.Linear(64, 64),
+                        nn.Tanh(),
+                        nn.Linear(64, 1)
+                    )
