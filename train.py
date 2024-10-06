815485385396b1a74db74af205ef9d34c32a352c
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-10-06 20:55:50 +0530

    training loop created

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 from const import *
 import gym
 import os
+import numpy as np
+import torch
+from PPO import PPO
+from datetime import datetime
 
 print("training environment name : " + env_name)
 
@@ -55,3 +59,106 @@ if not os.path.exists(directory):
 
 checkpoint_path = directory + "PPO_{}_{}_{}.pth".format(env_name, random_seed, run_num_pretrained)
 print("save checkpoint path : " + checkpoint_path)
+
+
+
+if random_seed:
+    print("--------------------------------------------------------------------------------------------")
+    print("setting random seed to ", random_seed)
+    torch.manual_seed(random_seed)
+    env.seed(random_seed)
+    np.random.seed(random_seed)
+
+
+ppo_agent = PPO(state_dim, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip, has_continuous_action_space, action_std)
+
+
+
+
+start_time = datetime.now().replace(microsecond=0)
+print("Started training at (GMT) : ", start_time)
+log_f = open(log_f_name,"w+")
+log_f.write('episode,timestep,reward\n')
+
+
+# printing and logging variables
+print_running_reward = 0
+print_running_episodes = 0
+
+log_running_reward = 0
+log_running_episodes = 0
+
+time_step = 0
+i_episode = 0
+
+while time_step <= max_training_timesteps:
+    
+    state = env.reset()
+    current_ep_reward = 0
+
+    for t in range(1, max_ep_len+1):
+        
+        # select action with policy
+        action = ppo_agent.select_action(state)
+        state, reward, done, frame = env.step(action)
+        
+        # saving reward and is_terminals
+        ppo_agent.buffer.rewards.append(reward)
+        ppo_agent.buffer.is_terminals.append(done)
+        
+        time_step +=1
+        current_ep_reward += reward
+
+        # update PPO agent
+        if time_step % update_timestep == 0:
+            ppo_agent.update()
+
+        # log in logging file
+        if time_step % log_freq == 0:
+
+            # log average reward till last episode
+            log_avg_reward = log_running_reward / log_running_episodes
+            log_avg_reward = round(log_avg_reward, 4)
+
+            log_f.write('{},{},{}\n'.format(i_episode, time_step, log_avg_reward))
+            log_f.flush()
+
+            log_running_reward = 0
+            log_running_episodes = 0
+
+        # printing average reward
+        if time_step % print_freq == 0:
+
+            # print average reward till last episode
+            print_avg_reward = print_running_reward / print_running_episodes
+            print_avg_reward = round(print_avg_reward, 2)
+
+            print("Episode : {} \t\t Timestep : {} \t\t Average Reward : {}".format(i_episode, time_step, print_avg_reward))
+
+            print_running_reward = 0
+            print_running_episodes = 0
+            
+        # save model weights
+        if time_step % save_model_freq == 0:
+            print("--------------------------------------------------------------------------------------------")
+            print("saving model at : " + checkpoint_path)
+            ppo_agent.save(checkpoint_path)
+            print("model saved")
+            print("Elapsed Time  : ", datetime.now().replace(microsecond=0) - start_time)
+            print("--------------------------------------------------------------------------------------------")
+            
+        # break; if the episode is over
+        if done:
+            break
+
+    print_running_reward += current_ep_reward
+    print_running_episodes += 1
+
+    log_running_reward += current_ep_reward
+    log_running_episodes += 1
+
+    i_episode += 1
+
+
+log_f.close()
+#env.close()
