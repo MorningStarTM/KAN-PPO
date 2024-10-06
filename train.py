46a07af527213487d681cc563ba8f82280048431
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-10-06 20:44:05 +0530

    folder creation added

---
+++ train.py
@@ -0,0 +1,42 @@
+from const import *
+import gym
+import os
+
+print("training environment name : " + env_name)
+
+env = gym.make(env_name)
+
+# state space dimension
+state_dim = env.observation_space.shape[0]
+
+# action space dimension
+if has_continuous_action_space:
+    action_dim = []
+else:
+    action_dim = env.action_space.n
+
+
+print(f"state dim : {state_dim}")
+print(f"action dim : {action_dim}")
+
+
+
+# Folder creation
+log_dir = "result/PPO_logs"
+if not os.path.exists(log_dir):
+      os.makedirs(log_dir)
+
+log_dir = log_dir + '/' + env_name + '/'
+if not os.path.exists(log_dir):
+      os.makedirs(log_dir)
+
+run_num = 0
+current_num_files = next(os.walk(log_dir))[2]
+run_num = len(current_num_files)
+
+
+#### create new log file for each run 
+log_f_name = log_dir + '/PPO_' + env_name + "_log_" + str(run_num) + ".csv"
+
+print("current logging run number for " + env_name + " : ", run_num)
+print("logging at : " + log_f_name)
